1f7dd59618fef339389bda441f2e2b56d4567c41
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-04-01 08:55:09 -0300

    feat: Implement Category Controller index feature

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use Core\UseCase\Category\{
+    CreateCategoryUseCase,
+    ListCategoriesUseCase
+};
+use Core\UseCase\DTO\Category\CreateCategoryInputDto;
+use Core\UseCase\DTO\Category\ListCategoriesInputDto;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request, ListCategoriesUseCase $useCase)
+    {
+        $response = $useCase->execute(
+            input: new ListCategoriesInputDto(
+                filter: $request->get('filter', ''),
+                orderColumn: $request->get('orderColumn', 'id'),
+                order: $request->get('order', 'DESC'),
+                page: (int) $request->get('page', 1),
+                pageLimit: (int) $request->get('pageLimit', 15),
+            )
+        );
+
+        return CategoryResource::collection(collect($response->items))
+            ->additional([
+                'meta' => [
+                    'total' => $response->total,
+                    'first_page' => $response->first_page,
+                    'current_page' => $response->current_page,
+                    'last_page' => $response->last_page,
+                    'per_page' => $response->per_page,
+                    'to' => $response->to,
+                    'from' => $response->from,
+                ]
+            ]);
+    }
+
+    public function show()
+    {
+    }
+
+    public function store(StoreCategoryRequest $request, CreateCategoryUseCase $useCase)
+    {
+        $response = $useCase->execute(
+            new CreateCategoryInputDto(
+                name: $request->name,
+                description: $request->description ?? '',
+                isActive: (bool) $request->is_active ?? true,
+            )
+        );
+
+        return (new CategoryResource(collect($response)))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function update()
+    {
+    }
+
+    public function destroy()
+    {
+    }
+}
