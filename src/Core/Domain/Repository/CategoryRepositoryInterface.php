6ee9e9a7128d792a9153d67b82cc48d9680db800
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-03-31 21:58:24 -0300

    refactor: lint paginate method

--- src/Core/Domain/Repository/CategoryRepositoryInterface.php
+++ src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -7,7 +7,13 @@ use Core\Domain\Entity\Category;
 interface CategoryRepositoryInterface
 {
     public function findAll(string $filter = '', string $orderColumn = 'id', string $order = 'DESC'): array;
-    public function paginate(string $filter = '', string $orderColumn = 'id', string $order = 'DESC', int $page = 1, int $pageLimit = 15): PaginationInterface;
+    public function paginate(
+        string $filter = '',
+        string $orderColumn = 'id',
+        string $order = 'DESC',
+        int $page = 1,
+        int $pageLimit = 15
+    ): PaginationInterface;
     public function findById(string $id): Category;
     public function insert(Category $category): Category;
     public function update(Category $category): Category;
